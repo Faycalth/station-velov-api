08eb31b9d89dbaff8a0b050dfbe4cc410d707219
Faycalth <faycalthamri38@gmail.com>
2019-12-13 16:49:18 +0100

    utilisation api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json?maxfeatures=100&start=1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result['values'])
+          this.setState({
+            isLoaded: true,
+            items: result['values']
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Veuillez attendre la fin du chargement (ça ne sera pas long, promis!)…</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.commune}>
+              {item.commune} : {item.nom} ({item.adresse})  
+            </li>
+          ))}
+        </ul>
+      );
+    }
+}}
 
 export default App;
